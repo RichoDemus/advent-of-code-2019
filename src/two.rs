13f8dbcb5f53bf85d05cb7b3b4a86f1ad4cacf75
Richard Tjerngren <rtjerngren@spotify.com>
2019-12-04 14:54:54 +0100

    Finished part 1 of day 2

--- src/two.rs
+++ src/two.rs
@@ -1,37 +1,91 @@
 use itertools::Itertools;
+use std::cmp;
 
 const ORIGINAL_INPUT: &str = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,13,19,1,9,19,23,2,13,23,27,2,27,13,31,2,31,10,35,1,6,35,39,1,5,39,43,1,10,43,47,1,5,47,51,1,13,51,55,2,55,9,59,1,6,59,63,1,13,63,67,1,6,67,71,1,71,10,75,2,13,75,79,1,5,79,83,2,83,6,87,1,6,87,91,1,91,13,95,1,95,13,99,2,99,13,103,1,103,5,107,2,107,10,111,1,5,111,115,1,2,115,119,1,119,6,0,99,2,0,14,0";
 const MODIFIED_INPUT: &str = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,13,19,1,9,19,23,2,13,23,27,2,27,13,31,2,31,10,35,1,6,35,39,1,5,39,43,1,10,43,47,1,5,47,51,1,13,51,55,2,55,9,59,1,6,59,63,1,13,63,67,1,6,67,71,1,71,10,75,2,13,75,79,1,5,79,83,2,83,6,87,1,6,87,91,1,91,13,95,1,95,13,99,2,99,13,103,1,103,5,107,2,107,10,111,1,5,111,115,1,2,115,119,1,119,6,0,99,2,0,14,0";
 
 fn exec(intcode: &str) -> String {
-    let codes: Vec<i32> = str_to_vec(intcode);
+    let codes: Vec<usize> = str_to_vec(intcode);
     let mut intputer = Intputer::from(codes);
 
     println!("Intputer:{:?}", intputer);
+    while intputer.process() == false {}
     intputer.get_memory()
 }
 
 #[derive(Debug)]
 struct Intputer {
-    memory: Vec<i32>
+    memory: Vec<usize>,
+    instruction_row: usize, //which "row" of instructions we're processing
 }
 
 impl Intputer {
-    fn from(codes: Vec<i32>) -> Intputer {
-        Intputer { memory: codes }
+    fn from(codes: Vec<usize>) -> Intputer {
+        Intputer {
+            memory: codes,
+            instruction_row: 0,
+        }
     }
 
     fn get_memory(self) -> String {
         self.memory.iter()
             .cloned()
-            .map(|val|val.to_string())
+            .map(|val| val.to_string())
             .join(",")
+    }
+
+    // process the next row, returns true if we're done
+    fn process(&mut self) -> bool {
+        let first_instruction = self.instruction_row * 4;
+        let last_instruction = cmp::min(self.instruction_row * 4 + 4, self.memory.len());
+        println!("Reading instructions from {} to {}", first_instruction, last_instruction);
+        let instructions = &self.memory[first_instruction..last_instruction];
+        println!("instructions: {:?}", instructions);
+        self.instruction_row += 1;
+        if let Some(99) = instructions.first() {
+            return true;
+        }
+
+        let opcode = instructions.first().expect("couldn't get operation");
+        let first_position = instructions.get(1).expect("couldn't get first pointer");
+        let second_position = instructions.get(2).expect("couldn't get second pointer");
+        let result_position = instructions.get(3).expect("couldn't get result pointer");
+
+        println!("Processing code {}, pos {} and {}, result to {}", opcode, first_position, second_position, result_position);
+        match opcode {
+            1 => {
+                let first_value = self.get_value(*first_position);
+                let second_value = self.get_value(*second_position);
+                println!("adding {} and {} (pos {} and {}) and writing it to {}", first_value, second_value, first_position, second_position, result_position);
+                let result = first_value + second_value;
+                self.write(*result_position, result)
+            },
+            2 => {
+                let first_value = self.get_value(*first_position);
+                let second_value = self.get_value(*second_position);
+                println!("multiplying {} with {} (pos {} and {}) and writing it to {}", first_value, second_value, first_position, second_position, result_position);
+                let result = first_value * second_value;
+                self.write(*result_position, result)
+            },
+            _ => panic!("unexpected opcode: {}", opcode),
+        }
+
+        false
+    }
+
+    fn get_value(&self, index:usize) -> usize {
+        self.memory.get(index).expect("could not get value from memory").clone()
+    }
+
+    fn write(&mut self, position:usize, value:usize) {
+        let old = std::mem::replace(&mut self.memory[position], value);
+        println!("Replaced {} with {} at {}", old, value, position)
     }
 }
 
-fn str_to_vec(str: &str) -> Vec<i32> {
+fn str_to_vec(str: &str) -> Vec<usize> {
     str.split(",")
-        .map(|code| code.parse::<i32>().expect("failed to parse to int"))
+        .map(|code| code.parse::<usize>().expect("failed to parse to int"))
         .collect()
 }
 
